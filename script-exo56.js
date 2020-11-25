a0310480f590a6adc1e95f783876ec4b7e65d2c6
Sylvie B <unsolo@hotmail.fr>
2020-11-25 14:19:43 +0100

    correction des ""

--- script-exo56.js
+++ script-exo56.js
@@ -2,30 +2,30 @@ let quest = prompt("Vous utilisez un(e) ?");
 console.log(quest)
 
 switch (quest) {
-    case souris:
-        alert("J'utilise une " + "quest");
-        break;
-
-    case clavier:
-        alert("J'utilise un " + quest);
-        break;
-
-    case ordinateur:
-        alert("J'utilise un " + quest);
-        break;
-
-    case tapis:
-        alert("J'utilise un " + quest);
-        break;
-
-    case imprimante:
+    case "souris":
         alert("J'utilise une " + quest);
         break;
 
-    case ondulateur:
+    case "clavier":
+        alert("J'utilise un " + quest);
+        break;
+
+    case "ordinateur":
+        alert("J'utilise un " + quest);
+        break;
+
+    case "tapis":
+        alert("J'utilise un " + quest);
+        break;
+
+    case "imprimante":
+        alert("J'utilise une " + quest);
+        break;
+
+    case "ondulateur":
         alert("J'utilise une " + quest);
         break;
 
     default:
-        alert("objet inconu")
+        alert("J'utilise un autre accessoire");
 }
